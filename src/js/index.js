4d9ffb0945889f4fe6cd0d223c52ac6ef1affb4b
Daniela Vazquez <daniela_056@hotmail.com>
2019-12-05 14:02:28 -0600

    Added const and function setDate() to update seconds mins and hour, finished project

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //On loaded
-});
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();  //gives us seconds of current minute
+  const secondsDegrees = ((seconds / 60) * 360) + 90; //we make 60 seconds a base of 360 for degrees
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((mins / 60) * 30) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+  hourHand.style.transform = `rotate (${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+
+setDate();
